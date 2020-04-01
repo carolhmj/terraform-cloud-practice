9fb76c9e42d63f956883db0c6c103cb26f39e988
Carolina <carolhmj@gmail.com>
2020-04-01 20:52:14 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "google" {
+    project = "amigosecreto-272618"
+    region  = "us-central1"
+    zone    = "us-central1-c"
+    version = "~> 3.14"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+    name = var.bucket-name
+}
