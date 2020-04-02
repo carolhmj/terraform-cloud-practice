df3829a588c8dbeda4363c5c1fe31458b038f9ce
Carolina <carolhmj@gmail.com>
2020-04-01 21:03:43 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "google" {
     region  = "us-central1"
     zone    = "us-central1-c"
     version = "~> 3.14"
+    credentials = var.credentials
 }
 
 resource "google_storage_bucket" "my_bucket" {
